db1a4c8041efac22402000e0a9464234b442bf88
André Carvalho <asantostc@gmail.com>
2017-06-03 18:57:26 -0300

    malloc: properly reuse freed block

--- ch7/malloc.c
+++ ch7/malloc.c
@@ -14,6 +14,22 @@ typedef struct block {
 
 static block_t *head = NULL;
 
+void fl_remove(block_t *b)
+{
+    if (!b->prev) {
+        if(b->next) {
+            head = b->next;
+        } else {
+            head = NULL;
+        }
+    } else {
+        b->prev->next = b->next;
+    }
+    if (b->next) {
+        b->next->prev = b->prev;
+    }
+}
+
 // print debug stats
 void stats(char *prefix)
 {
@@ -21,8 +37,8 @@ void stats(char *prefix)
     block_t *ptr = head;
     printf("[%s] free list: \n", prefix);
     int c = 0;
-    while (ptr != NULL) {
-        printf("(%d) [%10p] size: %ld\n", c, ptr, ptr->size);
+    while (ptr) {
+        printf("(%d) [%10p] -> [%10p] (size: %ld)\n", c, ptr, ptr + ptr->size, ptr->size);
         ptr = ptr->next;
         c++;
     }
@@ -33,6 +49,8 @@ void *_malloc(size_t size)
     block_t *ptr = head;
     while (ptr) {
         if (ptr->size == size) {
+            fl_remove(ptr);
+            printf("reusing previously freed block: %10p\n", BLOCK_MEM(ptr));
             return BLOCK_MEM(ptr);
         }
         ptr = ptr->next;
@@ -41,15 +59,16 @@ void *_malloc(size_t size)
     ptr = sbrk(sizeof(block_t) + size);
     if (!ptr) {
         printf("failed to alloc %ld", sizeof(block_t)+size);
-        exit(-1);
+        return NULL;
     }
     ptr->size = size;
+    ptr->next = NULL;
+    ptr->prev = NULL;
     return BLOCK_MEM(ptr);
 }
 
 void _free(void *ptr) 
 {
-    stats("free - start");
     block_t *block = BLOCK_HEADER(ptr);
     block->prev = NULL;
     if(head) {
@@ -60,18 +79,23 @@ void _free(void *ptr)
         block->next = NULL;
         head = block;
     }
-    stats("free - end");
 }
 
 int main(int argc, char const *argv[])
 {
-    stats("start");
-    char *str;
-    stats("malloc - start");
+    char *str, *str2;
     str = (char *) _malloc(15);
-    stats("malloc - end");
+    str2 = (char *) _malloc(15);
     strcpy(str, "tutorialspoint");
-    printf("String = %s,  Address = %u\n", str, *str);
+    strcpy(str2, "tutorial");
+    printf("String = %s,  Address = %p\n", str, &str);
+    printf("String = %s,  Address = %p\n", str2, &str2);
     _free(str);
+    stats("1");
+    str = (char *) _malloc(15);
+    stats("2");
+    _free(str2);
+    _free(str);
+    stats("3");
     return(0);
 }
