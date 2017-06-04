53ec8e02c158cf1e20d859fa26cbf691895e4a63
André Carvalho <asantostc@gmail.com>
2017-06-04 16:34:01 -0300

    malloc: release memory back to the OS in chuncks

--- ch7/malloc.c
+++ ch7/malloc.c
@@ -13,6 +13,10 @@ typedef struct block {
 #define ALLOC_UNIT 3 * sysconf(_SC_PAGESIZE)
 #endif
 
+#ifndef MIN_DEALLOC
+#define MIN_DEALLOC 1 * sysconf(_SC_PAGESIZE)
+#endif
+
 #define BLOCK_MEM(ptr) ((void *)((unsigned long)ptr + sizeof(block_t)))
 #define BLOCK_HEADER(ptr) ((void *)((unsigned long)ptr - sizeof(block_t)))
 
@@ -60,19 +64,36 @@ void scan_merge()
 {
     block_t *curr = head;
     unsigned long header_curr, header_next;
+    unsigned long program_break = (unsigned long) sbrk(0);
+    if (program_break == 0) {
+        printf("failed to retrieve program break\n");
+        return;
+    }
     while(curr->next) {
         header_curr = (unsigned long) curr;
         header_next = (unsigned long) curr->next;
         if (header_curr + curr->size + sizeof(block_t) == header_next) {
-            printf("merging continuous blocks: %ld <-> %ld\n", header_curr, header_next);
+            printf("merging continuous blocks: %10p (size: %ld) <-> %10p (size: %ld) \n", curr, curr->size, curr->next, curr->next->size);
             curr->size += curr->next->size + sizeof(block_t);
             curr->next = curr->next->next;
             if (curr->next) {
                 curr->next->prev = curr;
+            } else {
+                break;
             }
         }
         curr = curr->next;
-    } 
+    }
+    stats("after merge"); 
+    header_curr = (unsigned long) curr;
+    printf("comparing last mem block %10p (size: %ld) to program break %10p\n", curr, curr->size, (void *)program_break);
+    if (header_curr + curr->size + sizeof(block_t) == program_break && curr->size >= MIN_DEALLOC) {
+        printf("releasing %ld bytes to OS\n", curr->size + sizeof(block_t));
+        fl_remove(curr);
+        if (brk(curr) != 0) {
+            printf("error freeing memory\n");
+        }
+    }
 }
 
 // print debug stats
@@ -83,7 +104,7 @@ void stats(char *prefix)
     printf("[%s] free list: \n", prefix);
     int c = 0;
     while (ptr) {
-        printf("(%d) <%10p> (size: %ld)\n", c, BLOCK_MEM(ptr), ptr->size);
+        printf("(%d) <%10p> (size: %ld)\n", c, ptr, ptr->size);
         ptr = ptr->next;
         c++;
     }
@@ -94,10 +115,11 @@ void stats(char *prefix)
 block_t *split(block_t *b, size_t size) 
 {
     void *mem_block = BLOCK_MEM(b);
-    printf("splitting block at: %10p\n", mem_block);
+    printf("splitting block at: %10p (size: %ld)\n", b, b->size);
     block_t *newptr = (block_t *)((unsigned long)mem_block + size);
-    newptr->size = b->size - size;
+    newptr->size = b->size - (size + sizeof(block_t));
     b->size = size;
+    printf("created block at: %10p (size: %ld)\n", newptr, newptr->size);
     return newptr;
 }
 
@@ -126,12 +148,12 @@ void *_malloc(size_t size)
     printf("unable to find available block. allocating: %ld bytes\n", alloc_size);
     ptr = sbrk(alloc_size);
     if (!ptr) {
-        printf("failed to alloc %ldzn", alloc_size);
+        printf("failed to alloc %ld\n", alloc_size);
         return NULL;
     }
     ptr->next = NULL;
     ptr->prev = NULL;
-    ptr->size = alloc_size;
+    ptr->size = alloc_size - sizeof(block_t);
     if (alloc_size > size + sizeof(block_t)) {
         newptr = split(ptr, size);
         fl_add(newptr);
@@ -142,23 +164,37 @@ void *_malloc(size_t size)
 void _free(void *ptr) 
 {
     fl_add(BLOCK_HEADER(ptr));
+    stats("before scan");
     scan_merge();
+}
+
+void _cleanup() 
+{
+    printf("cleaning memory up\n");
+    if (head) {
+        if (brk(head) != 0) {
+            printf("failed to cleanup memory");
+        }
+    }
+    head = NULL;
+    stats("_cleanup end");
 }
 
 int main(int argc, char const *argv[])
 {
+    atexit(_cleanup);
     printf("mem page size: %ld bytes\n", sysconf(_SC_PAGESIZE));
     printf("bytes allocated per malloc: %ld\n", ALLOC_UNIT);
-    stats("begin");
+    stats("begin main");
     char *str, *str2;
-    str = (char *) _malloc(15);
-    str2 = (char *) _malloc(15);
+    str = (char *) _malloc(1);
+    str2 = (char *) _malloc(1);
     _free(str);
     stats("1");
-    str = (char *) _malloc(10);
+    str = (char *) _malloc(2);
     stats("2");
     _free(str2);
     _free(str);
-    stats("3");
+    stats("end main");
     return(0);
 }
